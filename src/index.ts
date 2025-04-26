f13ac7ec9ed3228e15b75713b8781cdfc85750ec
Alejandro Silva <alejoyodax@proton.me>
2025-04-25 23:37:39 -0500

    creado servidor y conexión a base de datos

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,24 @@
-const text = "hello my friend"
-global.console.log(text);
+import express from "express";
+const { sequelize, User } = require("./config/connectionDB");
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+})
+
+app.listen(port, () => {
+    console.log(`--> Servidor desplegado y corriendo en: http://localhost:${port}`)
+})
+
+// Realizar la conexión a la base de datos
+try {
+    (async ()=>{
+        await sequelize.authenticate();
+        console.log('--> Connection has been established successfully.');
+    })()
+
+} catch (error) {
+    console.error('** ERROR AL CONCECTAR A LA BASE DE DATOS: ', error);
+}
