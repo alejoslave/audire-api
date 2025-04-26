547639a21d0f6e7e592969df965a04677a63cac9
Alejandro Silva <alejoyodax@proton.me>
2025-04-25 23:38:22 -0500

    configurar conexión a base de datos sqlite e incializar modelo user

---
+++ src/config/connectionDB.ts
@@ -0,0 +1,20 @@
+import { Sequelize } from "sequelize";
+import SQLite from 'sqlite3';
+import { initUserModel, User} from "../models/UserModel";
+
+// Generar conexión a la base de datos
+const sequelize = new Sequelize({
+    dialect: 'sqlite',
+    storage: './database.sqlite', // BASE DE DATOS EN CARPETA RAÍZ
+    sync: { force: true },
+    dialectOptions: {
+        // Your sqlite3 options here
+        // for instance, this is how you can configure the database opening mode:
+        mode: SQLite.OPEN_READWRITE | SQLite.OPEN_CREATE | SQLite.OPEN_FULLMUTEX,
+    },
+});
+
+// Inicializar modelos en base de datos (crear tablas)
+initUserModel(sequelize);
+
+export { sequelize, User };
