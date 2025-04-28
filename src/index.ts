e267b013e4c6e4fef7b490f488c8833d0bb17391
Alejandro Silva <alejoyodax@proton.me>
2025-04-28 00:27:58 -0500

    añadir ruta de creación de usuario

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,48 @@
 import express from "express";
+import {Sequelize} from "sequelize";
+import {Json} from "sequelize/lib/utils";
 const { sequelize, User } = require("./config/connectionDB");
 
 const app = express();
 const port = 3000;
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+
+app.route('/user')
+    // Recuperar información del usuario
+    .get(async (req, res): Promise<void> => {
+
+        const queryResponse: typeof User = await User.findOne({
+            where: {name: req.query.name}
+        })
+
+        if(!queryResponse) console.log(`Usuario ${req.query.name} no encontrado`);
+        else console.log(`Usuario ${req.query.name} encontrado`);
+
+        res.send(queryResponse);
+}).post(async (req, res) => {
+    const userData  = req.body;
+
+    let user: typeof User;
+    let isCreated: boolean;
+    [user, isCreated] =  await User.findOrCreate({
+        where: {name: userData.name},
+        defaults: userData
+    });
+
+    isCreated ?
+        console.log("El usuario ha sido creado") :
+        console.log("El usuario ya existe y no ha sido creado");
+    user.isCreated = isCreated;
+
+    res.send({
+        user,
+        isCreated
+    })
+})
+
+
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
